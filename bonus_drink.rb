e5cbfedc41818526075a79d9cf42dd493b2711ba
NAKATANI Ichiro <nobitahouse@gmail.com>
2016-10-06 22:04:40 +0900

    完成

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,13 @@
+# coding: utf-8
+# frozen_string_literal: true
 class BonusDrink
-  def self.total_count_for(amount)
-    0
+  X = 3
+  def self.total_count_for(drinks, carried_bottles = 0)
+    drinks = drinks.to_i
+    return 0 if drinks < 1
+    total_bottles = drinks + carried_bottles
+    return drinks if total_bottles < X
+    bonus_drinks, next_carried_bottles = total_bottles.divmod(X)
+    drinks + total_count_for(bonus_drinks, next_carried_bottles)
   end
 end
